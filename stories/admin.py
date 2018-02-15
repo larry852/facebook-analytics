cdf1a1faed07d50e071676f28610ca3d83425786
larry852 <larryportocarrero@gmail.com>
2018-02-14 20:36:37 -0500

    Eliminacion de columnas inecesarias

--- stories/admin.py
+++ stories/admin.py
@@ -5,10 +5,10 @@ from django.utils.safestring import mark_safe
 
 @admin.register(Storie)
 class AdminStorie(admin.ModelAdmin):
-    list_display = ('post', 'message', 'shares', 'reaction_total')
+    list_display = ('post', 'message')
     actions = ['delete']
     list_display_links = None
-    ordering = ('-reaction__count', '-shares')
+    # ordering = ('-reaction__count', '-shares')
 
     def has_add_permission(self, request):
         return False
@@ -19,60 +19,11 @@ class AdminStorie(admin.ModelAdmin):
     post.short_description = 'post'
     post.admin_order_field = 'id'
 
-    def url_html_entity(self, obj):
-        return mark_safe('<a target="_blank" href="https://www.facebook.com/{}"> {} </a>'.format(obj.entity.fb_id, obj.entity.name))
-
-    url_html_entity.short_description = 'page'
-    url_html_entity.admin_order_field = 'entity'
-
-    def description(self, obj):
-        return obj.attachment.description
-
-    description.short_description = 'description'
-    description.admin_order_field = 'attachment'
-
     def message(self, obj):
         return obj.attachment.message
 
     message.short_message = 'message'
     message.admin_order_field = 'attachment'
-
-    def reaction_total(self, obj):
-        return Reaction.objects.get(storie=obj, type='NONE').count
-
-    reaction_total.admin_order_field = 'reaction__count'
-
-    def reaction_haha(self, obj):
-        return Reaction.objects.get(storie=obj, type='HAHA').count
-
-    def reaction_like(self, obj):
-        return Reaction.objects.get(storie=obj, type='LIKE').count
-
-    def reaction_love(self, obj):
-        return Reaction.objects.get(storie=obj, type='LOVE').count
-
-    def reaction_wow(self, obj):
-        return Reaction.objects.get(storie=obj, type='WOW').count
-
-    def reaction_sad(self, obj):
-        return Reaction.objects.get(storie=obj, type='SAD').count
-
-    def reaction_angry(self, obj):
-        return Reaction.objects.get(storie=obj, type='ANGRY').count
-
-    def reaction_thankful(self, obj):
-        return Reaction.objects.get(storie=obj, type='THANKFUL').count
-
-    def get_query_html(self, obj):
-        querys = obj.query.url.split('/')
-        html = ''
-        for query in querys:
-            if query:
-                html += '<li> {} </li>'.format(query)
-        return mark_safe(html)
-
-    get_query_html.short_description = 'query'
-    get_query_html.admin_order_field = 'query__url'
 
 
 @admin.register(Comment)
